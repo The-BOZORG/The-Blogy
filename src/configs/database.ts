bc027bb36d6489c58c3910f1ed4862e760274056
soroush <soroushfalaki24@hotmail.com>
2026-08-23 18:05:08 +0330

    setup db and prisma

---
+++ src/configs/database.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from 'generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import pkg from 'pg';
+
+const pool = new pkg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({
+  adapter,
+  log:
+    process.env.NODE_ENV === 'development'
+      ? ['query', 'error', 'warn']
+      : ['error'],
+});
+
+export const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log('DB connect via prisma');
+  } catch (error) {
+    console.error(`DB connect error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export const disconnectDB = async () => {
+  await prisma.$disconnect();
+};
