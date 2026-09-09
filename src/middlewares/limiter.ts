b5ac64717c65423007335a892f1e6feb845719ef
Soroush Falaki <soroushfalaki24@hotmail.com>
2026-09-09 09:00:53 +0330

    fix some bugs

--- src/middlewares/limiter.ts
+++ src/middlewares/limiter.ts
@@ -1,41 +1,23 @@
 import { rateLimit } from 'express-rate-limit';
-import { RedisStore } from 'rate-limit-redis';
-import { redisClient } from '@/configs/redis';
 
 export const authLimiter = rateLimit({
   windowMs: 60 * 1000,
   limit: 50,
-
   standardHeaders: 'draft-8',
   legacyHeaders: false,
 
-  store: new RedisStore({
-    sendCommand: (...args: string[]) => {
-      return redisClient.sendCommand(args);
-    },
-    prefix: 'rate-limit:1m:',
-  }),
-
   message: {
-    error: 'Too many requests. Please try again later.',
+    error: 'too many requests. please try again later.',
   },
 });
 
 export const globalLimiter = rateLimit({
   windowMs: 3 * 60 * 1000,
   limit: 100,
-
   standardHeaders: 'draft-8',
   legacyHeaders: false,
 
-  store: new RedisStore({
-    sendCommand: (...args: string[]) => {
-      return redisClient.sendCommand(args);
-    },
-    prefix: 'rate-limit:3m:',
-  }),
-
   message: {
-    error: 'Too many requests. Please try again later.',
+    error: 'too many requests. please try again later.',
   },
 });
